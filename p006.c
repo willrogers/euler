d71fe759423957f6d1579fb525fa28f0ba0345da
Will Rogers <wahrogers@gmail.com>
2013-12-31 15:11:29 +0000

    Simple solution to problem 6.

---
+++ p006.c
@@ -0,0 +1,18 @@
+/* Find the difference between the sum of the squares and the square of 
+   the sum of the first 100 natural numbers.
+*/
+
+#define MAX 100
+#include <stdio.h>
+
+int main(char* args[]) {
+	int i = 0;
+	int sum_squares = 0;
+	int sum = 0;
+	for (i = 1;  i < MAX + 1; i++) {
+		sum += i;
+		sum_squares += i*i;
+	}
+	printf("Sum is %d\n", sum);
+	printf("Answer: %d\n", (sum*sum - sum_squares));
+}
