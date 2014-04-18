c363d6e72e57a2a05194419d3084bcf233146f87
Will Rogers <wahrogers@gmail.com>
2014-04-18 16:24:50 +0100

    Completed and tidied C version of problem 13.

--- c/p013.c
+++ c/p013.c
@@ -5,16 +5,17 @@
 #include <stdio.h>
 #include <limits.h>
 
+/* Details of data file */
 #define DATA_FILE "../data/longs.txt"
 #define LINES 100
 #define NUM_LENGTH 50
+/* Used for each part of largeint */
 #define LONG_DIGITS 10
 #define MAX_LONG 10000000000
 
 /* define a struct to hold a very large integer */
 struct largeint {
-
-	/* Each long holds ten digits.  Lowest values first*/
+	/* Each long holds ten digits, base 10.  Lowest values first. */
 	long l1;
 	long l2;
 	long l3;
@@ -23,20 +24,51 @@ struct largeint {
 	long l6;
 };
 
+struct largeint new_largeint() {
+	struct largeint l;
+	l.l1 = 0L;
+	l.l2 = 0L;
+	l.l3 = 0L;
+	l.l4 = 0L;
+	l.l5 = 0L;
+	l.l6 = 0L;
+	return l;
+}
+
 void print_largeint(struct largeint * l) {
-	printf("%10ld%10ld%10ld%10ld%10ld%10ld\n", l->l6, l->l5, l->l4, l->l3, l->l2, l->l1); 
+	printf("%10ld%010ld%010ld%010ld%010ld%010ld\n", 
+		l->l6, l->l5, l->l4, l->l3, l->l2, l->l1); 
 }
 
 struct largeint add_largeint(struct largeint li1, struct largeint li2) { 
 	struct largeint li;
 	long l;
+	int m = 0;
 	
 	l = li1.l1 + li2.l1;
-	if (l > MAX_LONG) { 
-		long remainder = l % MAX_LONG;
-		int m = l / MAX_LONG;
-	}
+	m = l / MAX_LONG;
+	l = l % MAX_LONG;
+	li.l1 = l;
+	l = li1.l2 + li2.l2 + m;
+	m = l / MAX_LONG;
+	l = l % MAX_LONG;
+	li.l2 = l;
+	l = li1.l3 + li2.l3 + m;
+	m = l / MAX_LONG;
+	l = l % MAX_LONG;
+	li.l3 = l;
+	l = li1.l4 + li2.l4 + m;
+	m = l / MAX_LONG;
+	l = l % MAX_LONG;
+	li.l4 = l;
+	l = li1.l5 + li2.l5 + m;
+	m = l / MAX_LONG;
+	l = l % MAX_LONG;
+	li.l5 = l;
+	/* we assume l6 won't overflow */
+	li.l6 = li1.l6 + li2.l6 + m;
 	
+	return li;
 }
 
 long mul_ten_digits(int * array) { 
@@ -49,49 +81,44 @@ long mul_ten_digits(int * array) {
 	return current;
 }
 
-void parse_file(long long * longs) {
+void parse_file(struct largeint * array) {
 	FILE * f = fopen(DATA_FILE, "r");
 	
 	char c;
-	long long current = 0;
 	int digit;
 	int i, j;
 
 	int num[NUM_LENGTH];
-
 	/* we know the exact format of the file */
 	for (i = 0; i < LINES; i++) { 
 		struct largeint lint;
 		for (j = 0; j < NUM_LENGTH; j++) {
 			c = fgetc(f);
 			digit = c - '0';
-			printf("%d", digit);
 			num[j] = digit;
 		}
-		printf("\n");
 		lint.l5 = mul_ten_digits(&num[0]);
 		lint.l4 = mul_ten_digits(&num[10]);
 		lint.l3 = mul_ten_digits(&num[20]);
 		lint.l2 = mul_ten_digits(&num[30]);
 		lint.l1 = mul_ten_digits(&num[40]);
 		lint.l6 = 0;
-		print_largeint(&lint);
+		array[i] = lint;
 
 		c = fgetc(f); // newline
-		
 	}
-	
 	fclose(f);
-		
 }
 
 int main() { 
 	long long longs[LINES];
-	parse_file(&longs[0]);
-	printf("sizeof int: %lu.\n", sizeof(int));
-	printf("sizeof long: %lu.\n", sizeof(long));
-	printf("sizeof long long: %lu.\n", sizeof(long long));
-	printf("Max long long: %llu.\n", LLONG_MAX);
-}
+	struct largeint lints[LINES];
+	parse_file(&lints[0]);
 
-	
+	int i;
+	struct largeint total = new_largeint();
+	for(i = 0; i < LINES; i++) {
+		total = add_largeint(total, lints[i]);	
+	}
+	print_largeint(&total);
+}
