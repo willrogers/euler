de87cb2acb2689a5f328944e253eba15dd3d484a
Will Rogers <wahrogers@gmail.com>
2014-07-07 21:43:30 +0100

    Tidy Haskell p4.

--- haskell/p004.hs
+++ haskell/p004.hs
@@ -1,7 +1,6 @@
 
-str1 = "22"
 
---pal :: [Char] -> Bool
+pal :: [Char] -> Bool
 pal [] = error "Empty list"
 pal [x] = True
 pal (x:xs) = if xs == [x] then True
@@ -9,7 +8,4 @@ pal (x:xs) = if xs == [x] then True
            else if (length xs) == 1 then False
            else False
 
-main = do print (pal str1)
-          print (pal "23454")
-          print (pal "2345432")
-          print (maximum ([ x*y | x <- [1..999], y <- [1..999], pal (show (x*y)) ]))
+main = do print (maximum ([ x*y | x <- [1..999], y <- [1..999], pal (show (x*y)) ]))
