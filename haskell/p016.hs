128e5678a4368ff970aaa11fd9e533ea60c19cc1
Will Rogers <wahrogers@gmail.com>
2014-07-15 19:37:16 +0100

    Haskell 16: move sumdig to Utils.

--- haskell/p016.hs
+++ haskell/p016.hs
@@ -1,12 +1,8 @@
 
+import Utils
 
+twotox :: Integer -> Integer
 twotox 0 = 1
 twotox n = 2 * twotox (n - 1)
-
-sumdig n 
-    | d == 0 = n
-    | otherwise = r + sumdig d
-    where d = n `div` 10
-          r = n `mod` 10
 
 main = do print $ sumdig $ twotox 1000
