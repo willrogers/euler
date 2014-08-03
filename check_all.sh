04facdae26689c5848a0d1fffe5d63fadcf0d055
Will Rogers <wahrogers@gmail.com>
2014-08-03 12:18:01 +0100

    Checking infrastructure includes python

--- check_all.sh
+++ check_all.sh
@@ -1,25 +1,57 @@
 #!/bin/bash
 
-PROBLEMS=400
-PDIR=haskell
+PROBLEMS=030
 LOG=compile.log
-LIBS=$PDIR/Utils.hs
 rm $LOG
+
+type=$1
+PDIR=$1
+LIBS=$PDIR/Utils.hs
+
+function compile_python {
+	if [ -f $PDIR/p$1.py ] ; then
+		return 0 
+	else
+		return 1
+	fi
+}
+
+function run_python {
+	python $PDIR/p$1.py 2>> $LOG
+}
+
+function compile_haskell {
+	if [ -f $PDIR/p$1.hs ] ; then
+		ghc $PDIR/p$1.hs -i $LIBS >> $LOG 2>&1
+		return $?
+	else
+		return 1
+	fi
+}
+
+function run_haskell {
+	haskell/p$1
+}
+
 
 correct=0
 for x in $(seq -w $PROBLEMS) ; do
-	if [ -f $PDIR/p$x.hs ] && ghc $PDIR/p$x.hs -i $LIBS >> $LOG 2>&1 ; then
-		start=$(date +%s)
-		ans=$(haskell/p$x)
-		end=$(date +%s)
-		duration=$(echo "$end-$start" | bc)
-		printf "Problem %3d: answer %12d " $(expr $x + 0) $ans 
-		result=incorrect
-		if ./checker.py $x $ans ; then 
-			result=correct
-			((correct=correct+1))
+	if eval "compile_$type $x"; then
+		st=$(date +%s)
+		if ans=$(eval "run_$type $x") ; then 
+		
+			end=$(date +%s)
+			duration=$(echo "$end-$st" | bc)
+			printf "Problem %3d: answer %12d " $(expr $x + 0) $ans 
+			result=incorrect
+			if ./checker.py $x $ans ; then 
+				result=correct
+				((correct=correct+1))
+			fi
+			printf "%10s %3ds\n" $result $duration
+		else
+			echo "Problem $x: runtime error"
 		fi
-		printf "%10s %3ds\n" $result $duration
 	fi
 done
 
