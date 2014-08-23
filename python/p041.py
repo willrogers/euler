48672d5c8fea32786a8ebebcaea2f2484dd35cb3
Will Rogers <wahrogers@gmail.com>
2014-08-23 12:18:17 +0100

    Tidy python problem 41.

--- python/p041.py
+++ python/p041.py
@@ -1,26 +1,20 @@
 '''
 Find the largest pandigital prime.
 
-To start with, assume that it has 9 digits.
+The sum of 8 and 9-digit pandigital numbers divides by three,
+so all the numbers do too and none are prime.
 
-NO!  Just work down from the top and find the first one!
-That didn't work, either.
-
-7652413 was the biggest - then they seemed to stop very abruptly.
-Is there a logical reason for this?
-Yes.  The sum of 8 and 9-digit pandigital numbers divides by three,
-so the numbers do too.
+Start with the biggest pandigital 7-digit number and work down.
 
 You could also create the pandigital numbers using combinatorics.
 '''
 from utils import is_pandigital, is_prime
 i = 7654321
-pan_primes = []
 
 while True:
     if is_pandigital(i):
         if is_prime(i):
-            exit()
+            break
     i -= 2
 
-print pan_primes
+print i
