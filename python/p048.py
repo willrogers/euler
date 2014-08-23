1e4cdddf1f3e35f895a3ad73c574d1ac62956504
Will Rogers <wahrogers@gmail.com>
2014-08-23 12:28:24 +0100

    Tidy and correct python problem 48.

--- python/p048.py
+++ python/p048.py
@@ -1,9 +1,11 @@
 """
 Find the last 10 digits of 1^1 + 2^2 + ... + 1000^1000
 
-The trick here is that you can always forget the leading digits because 
+The trick here is that you can always forget the leading digits because
 they won't contribute to the sum.
 """
+LIMIT = 1000
+
 def pow10to10(x, y):
     """
     Get the power, but each time truncate to 10^10 so that the numbers
@@ -11,11 +13,9 @@ def pow10to10(x, y):
     """
     tot = 1
     for i in range(y):
-        tot = (tot * x) % 10000000000
+        tot = (tot * x) % int(1e10)
     return tot
 
-tot = 0
-for i in range(1, 1001):
-    tot = tot + pow10to10(i, i)
+tot = sum(pow10to10(i+1, i+1) for i in range(LIMIT))
 
-print tot 
+print tot % int(1e10)
