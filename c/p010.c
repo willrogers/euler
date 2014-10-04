6e7d2e03a1428fc424760017675053b04e20b32f
Will Rogers <wahrogers@gmail.com>
2014-10-04 13:44:33 +0100

    Correct and tidy whitespace in C problem 10

--- c/p010.c
+++ c/p010.c
@@ -6,15 +6,14 @@
 #define UPPER 2000000
 
 int main(char* args[]) {
-	
-        long total = 2;
-        int i = 0;
-        for (i=3; i<UPPER; i+=2) {
-                if (is_prime(i)) {
-		//	printf("prime: %d\n", i);
-                        total += i;
-                }
-        }
-        printf("The total is %ld\n", total);
+	long total = 2;
+	int i = 0;
+	for (i=3; i<UPPER; i+=2) {
+		if (is_prime(i)) {
+			total += i;
+		}
+	}
+	printf("%ld\n", total);
+	return 0;
 }
 
