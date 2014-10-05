8e06f8d1a9dfcd9f50b6343e84af112f2313669b
Will Rogers <wahrogers@gmail.com>
2014-10-05 20:25:59 +0100

    Simplify Python problem 14.

--- python/p014.py
+++ python/p014.py
@@ -8,43 +8,21 @@ First attempt, after removing print statements and unnecessary function
 calls, took about a minute.
 '''
 
-import cProfile
-
-def shrink(n):
-    return n / 2
-
-def grow(n):
-    return 3 * n + 1
-
-def reduce(n):
-    l = []
-    while n != 1:
-        l.append(n)
-        if n % 2 == 0:
-#            n = shrink(n)
-            n = n / 2
-        else:
-            n = grow(n)
-    l.append(1)
-#    print l
-    return len(l)
-
-def sreduce(n):
+def collatz_num(n):
     l = 0
     while n != 1:
-        l += 1
         if n % 2 == 0:
             n = n / 2
         else:
             n = 3 * n + 1
-    l += 1
+        l += 1
     return l
 
 
 def run():
     max = (0,0)
     for i in range(1, 1000000):
-        j = sreduce(i)
+        j = collatz_num(i)
         if j > max[1]:
             max = (i,j)
     print max[0]
