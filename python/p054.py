98146f04648819713dc7913b75d889471aa7f681
Will Rogers <wahrogers@gmail.com>
2014-12-19 11:32:06 +0000

    Incomplete: p054 python

---
+++ python/p054.py
@@ -0,0 +1,92 @@
+import numpy
+
+HIGH = 0
+PAIR = 1
+TWO_PAIR = 2
+THREE = 3
+STRAIGHT = 4
+FLUSH = 5
+FULL_HOUSE = 6
+FOUR = 7
+STRAIGHT_FLUSH = 8
+ROYAL_FLUSH = 9
+
+HEARTS = 0
+SPADES = 1
+CLUBS = 2
+DIAMONDS = 3
+
+SUITS = {'H': HEARTS,
+         'S': SPADES,
+         'C': CLUBS,
+         'D': DIAMONDS}
+
+ROYALS = {'T': 10,
+          'J': 11,
+          'Q': 12,
+          'K': 13,
+          'A': 14}
+
+
+def classify_flush(hand):
+    print('Classfying %s' % hand)
+    if max(hand[:,0]) - min(hand[:,0]) == 4:
+        if max(hand[:,0]) == 14:
+            return ROYAL_FLUSH
+        else:
+            return STRAIGHT_FLUSH
+    else:
+        return FLUSH
+
+def classify_two_groups(hand):
+    pass
+
+def classify(hand):
+    if len(set(hand[:,1])) == 1:
+        return classify_flush(hand)
+    groups = len(set(hand[:,0]))
+    if (groups) == 2:
+        # full house, four in a row
+        return classify_two_groups(hand)
+        pass
+    if (groups) == 3:
+        # three, two pair
+        pass
+    if (groups) == 4:
+        # pair
+        pass
+
+
+def read_hand(strings):
+    hand = numpy.zeros([5,2])
+    for i, item in enumerate(strings):
+        try:
+            num = int(item[0])
+        except ValueError:
+            num = ROYALS[item[0]]
+        suit = SUITS[item[1]]
+        hand[i, 0] = num
+        hand[i, 1] = suit
+    # sort by card number
+    return numpy.sort(hand, axis=0)
+
+
+def load(filename):
+    hands = []
+    with open(filename) as f:
+        for line in f:
+            parts = line.split()
+            hand1 = read_hand(parts[0:5])
+            hand2 = read_hand(parts[5:10])
+            hands.append((hand1, hand2))
+    return hands
+
+hands = load('../data/p054_poker.txt')
+
+print(hands[0])
+print(len(hands))
+
+hand = [2,HEARTS,3,HEARTS,4,HEARTS,5,HEARTS,6,HEARTS]
+hand = numpy.array(hand).reshape(5,2)
+print hand
+print(classify(hand))
