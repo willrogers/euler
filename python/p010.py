2451da6a49ecfb61d4d8f1dc6a17b6733ad71600
Will Rogers <wahrogers@gmail.com>
2015-04-01 10:16:33 +0100

    Quicker approaches to python problem 10.

--- python/p010.py
+++ python/p010.py
@@ -1,20 +1,44 @@
 # Find the sum of all the primes below two million.
 import math
+import utils
+import numpy
 
-def is_prime(n):
-    if n == 1:
-        return False
-    max = int(math.sqrt(n)) + 1
-    for i in range(2,max):
-        if n % i == 0:
-            return False
-    return True
+LIMIT = 2000000
 
-total = 0
-for i in range(2000000):
-    if is_prime(i):
-        total += i
 
-print total 
+def naive():
+    total = 2
+    for i in range(3, LIMIT, 2):
+        if utils.is_prime(i):
+            total += i
+    return total
 
+
+def dict_cancel():
+    poss = range(2, LIMIT)
+    prime = {i: True for i in range(2, LIMIT)}
+    for i in [2] + range(3, LIMIT, 2):
+        count = i * 2
+        while count < LIMIT:
+            prime[count] = False
+            count += i
+
+    total = sum(x for x in prime.keys() if prime[x])
+    return total
+
+
+def array_cancel():
+    a = numpy.arange(LIMIT)
+    a[1] = 0
+    for i in [2] + range(3, LIMIT, 2):
+        count = i * 2
+        while count < LIMIT:
+            a[count] = 0
+            count += i
+
+    total = sum(a)
+    return total
+
+
+print(dict_cancel())
 
