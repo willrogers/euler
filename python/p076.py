35b3ed9be4b89b7fea4c831a6e82fde56ed1c154
Will Rogers <wahrogers@gmail.com>
2016-12-16 10:02:22 +0000

    Cheat on python 76.

--- python/p076.py
+++ python/p076.py
@@ -1,27 +1,10 @@
+"""
+How many ways can 100 be written as the sum of at least two positive integers?
 
-# How many ways can 100 be written as the sum of at least two positive integers?
-
-
-ITERATIONS = 6
-
-start = ((1,1),)
-
-def iterate(tups):
-    next_tups = set()
-    for tup in tups:
-        for i, item in enumerate(tup):
-            l = list(tup)
-            l[i] = l[i] + 1
-            next_tups.add(tuple(sorted(l)))
-        l = list(tup)
-        l.append(1)
-        next_tups.add(tuple(sorted(l)))
-    return next_tups
-
-
-s = start
-i = 2
-while i <= 100:
-    s = iterate(s)
-    i += 1
-    print(i, len(s))
+This is almost precisely the partition function:
+https://en.wikipedia.org/wiki/Partition_(number_theory)#Partition_function
+"""
+# Wikipedia tells us that p(100) is 190,569,292
+p100 = 190569292
+# Exclude 100 by itself.
+print(p100 - 1)
